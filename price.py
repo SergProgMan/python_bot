c9617c34e7867acdc17ad6d9f613f5d46eba8b30
SergProgMan <semibratyev@GMAIL.COM>
2018-06-07 11:38:44 +0300

    Update price.py
    add coments

--- price.py
+++ price.py
@@ -4,5 +4,5 @@ import requests
 res = requests.get('https://yobit.net/api/3/ticker/ltc_btc') # получаем данные ticker'а
 res_obj = json.loads(res.text) # переводим полученный текст в объект с данными
 
-print("SELL: %0.8f" % res_obj['ltc_btc']['sell'])
-print("BUY: %0.8f" % res_obj['ltc_btc']['buy'])
+print("SELL: %0.8f" % res_obj['ltc_btc']['sell']) #(лучшая цена продажи)
+print("BUY: %0.8f" % res_obj['ltc_btc']['buy']) #(лучшая цена покупки)
